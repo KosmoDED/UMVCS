688ce3d7c7d2b61bea1290eedd251d6db7d908f9
Samuel Asher Rivello <srivello@hotmail.com>
2019-07-17 00:56:45 -0500

    Add Audio

---
+++ UMCVS/Assets/Scripts/Runtime/RMC/Projects/MyBouncyBallExample/UMVCS/Controller/AudioController.cs
@@ -0,0 +1,27 @@
+﻿using RMC.Architectures.UMVCS.Model;
+using RMC.Architectures.UMVCS.Controller;
+using RMC.Projects.MyBouncyBallExample.UMVCS.Controller.Commands;
+using RMC.Architectures.UMVCS.Service;
+using RMC.Projects.MyBouncyBallExample.UMVCS.View;
+
+namespace RMC.Projects.MyBouncyBallExample.UMVCS.Controller
+{
+	/// <summary>
+	/// TODO: Add comment
+	/// </summary>
+	public class AudioController : BaseController<NullModel, AudioView, NullService>
+	{
+		private AudioView _audioView { get { return BaseView as AudioView; } }
+
+		protected void Start()
+		{
+			Context.CommandManager.AddCommandListener<BouncedCommand>(
+				CommandManager_OnBounced);
+
+		}
+		private void CommandManager_OnBounced(BouncedCommand e)
+		{
+			_audioView.PlayAudioClip();
+		}
+	}
+}
---
+++ UMCVS/Assets/Scripts/Runtime/RMC/Projects/MyBouncyBallExample/UMVCS/View/AudioView.cs
@@ -0,0 +1,23 @@
+﻿using RMC.Architectures.UMVCS.View;
+using UnityEngine;
+
+namespace RMC.Projects.MyBouncyBallExample.UMVCS.View
+{
+	/// <summary>
+	/// TODO: Add comment
+	/// </summary>
+	public class AudioView : BaseView
+	{
+		[SerializeField]
+		private AudioSource _audioSource;
+
+		[SerializeField]
+		private AudioClip _audioClip;
+
+		public void PlayAudioClip ()
+		{
+			_audioSource.clip = _audioClip;
+			_audioSource.Play();
+		}
+	}
+}
